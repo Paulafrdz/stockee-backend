72ef71e68103737981a1b25bc713dea43ac882d3
Paula Fernandez Aranda <paulafa8@gmail.com>
2026-03-31 10:58:58 +1100

    fix passwords

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT IGNORE INTO roles (id_role, name) VALUES (2, 'ROLE_USER');
 
 INSERT IGNORE INTO users (id, username, email, password)
 VALUES (1, 'paula', 'paulafa8@hotmail.com', 
-        '$2a$12$gO7qsqwroL6uHdRb7kKk3udxMtVBd9XIN0dMuTF.V.wMINyY3WYau');
+        '$2a$10$AUjjA2C5Pezduc4sPoRq7O8D0JEqWh7dJfI4qNu/Lb5glkyPyhOHK');
 
 INSERT IGNORE INTO roles_users (user_id, role_id)
 VALUES (1, 1); -- 1 = ROLE_ADMIN
